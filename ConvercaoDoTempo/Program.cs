d9a64035ad63bff45bfd955184701276fc1b511c
ivanmslima <81934424+ivanmslima@users.noreply.github.com>
2022-11-07 20:25:21 -0300

    Adicionar arquivos de projeto.

---
+++ ConvercaoDoTempo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int N, horas, resto, minutos, segundos;
+
+        Console.WriteLine("Digite os segundo que serão convertidos");
+
+        N = int.Parse(Console.ReadLine());
+
+        horas = N / 3600;
+        resto = N % 3600;
+
+        minutos = resto / 60;
+        segundos = resto % 60;
+
+        Console.WriteLine(horas + " H : " + minutos + " min : " + segundos + " seg ");
+
+        Console.ReadLine();
+
+
+
+
+
+    }
+
+}
